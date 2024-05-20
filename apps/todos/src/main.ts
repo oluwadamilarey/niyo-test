6613484149765a82344524fdc1a5cbab59db59d5
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2024-05-20 06:23:59 +0100

    bcrypt package upgrade

--- apps/todos/src/main.ts
+++ apps/todos/src/main.ts
@@ -5,14 +5,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(TodosModule);
-  const config = new DocumentBuilder()
-    .setTitle('Todo API')
-    .setDescription('API for managing todos')
-    .setVersion('1.0')
-    .addTag('todos')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Todo API')
+  //   .setDescription('API for managing todos')
+  //   .setVersion('1.0')
+  //   .addTag('todos')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
